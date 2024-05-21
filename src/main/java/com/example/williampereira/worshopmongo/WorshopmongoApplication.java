cc884b5eef5bacbd584d88a46c21d34203482af7
William Barbosa <wb.pereiraa@gmail.com>
2024-05-20 23:03:17 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/example/williampereira/worshopmongo/WorshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.williampereira.worshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorshopmongoApplication.class, args);
+	}
+
+}
