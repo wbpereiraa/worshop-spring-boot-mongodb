eb475c3c6c1b822c5328c9f93ca17da43d48b40b
William Barbosa <wb.pereiraa@gmail.com>
2024-05-24 06:53:47 -0300

    Obtendo um usuario pelo id

--- src/main/java/com/example/williampereira/worshopmongo/services/UserService.java
+++ src/main/java/com/example/williampereira/worshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.example.williampereira.worshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.williampereira.worshopmongo.domain.User;
 import com.example.williampereira.worshopmongo.repository.UserRepository;
+import com.example.williampereira.worshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,8 @@ public class UserService {
 		return repo.findAll();
 	}
 
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
